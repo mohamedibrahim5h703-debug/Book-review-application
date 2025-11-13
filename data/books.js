cfb43be3c247b9f5a5744819c7580a5e593db51b
mohamedibrahim5h703-debug <mohamed.ibrahim.5h703@gmail.com>
2025-11-13 17:19:00 +0200

    Create books.js

---
+++ data/books.js
@@ -0,0 +1,7 @@
+const books = {
+  "12345": { title: "Book A", author: "Author1", reviews: {} },
+  "23456": { title: "Book B", author: "Author2", reviews: {} },
+  "34567": { title: "Book C", author: "Author1", reviews: {} }
+};
+
+module.exports = books;
